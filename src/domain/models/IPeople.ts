048d10a70b9187a56d2e0eda59d1a98f4a4324f0
Ruben Giraldo <ru.giraldo@gmail.com>
2024-11-22 06:02:51 -0500

    Create people interface

---
+++ src/domain/models/IPeople.ts
@@ -0,0 +1,19 @@
+export interface IPeople {
+  id: string
+  nombre: string
+  altura: string
+  masa: string
+  color_de_cabello: string
+  color_de_piel: string
+  color_de_ojos: string
+  año_de_nacimiento: string
+  genero: string
+  planeta_natal: string
+  peliculas: string[]
+  especies: string[]
+  vehículos: string[]
+  naves_estelares: string[]
+  creado: string
+  editado: string
+  url: string
+}
